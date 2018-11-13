a5cbec20e4765956bf478459ab9ebd046e690604
mallorynolan <mallorynolan@s1020n93h024.wst2.mun.ca>
2018-11-13 15:35:01 -0330

    Created Methods for StatsData

--- StatsData.java
+++ StatsData.java
@@ -1,5 +1,30 @@
 
 public class StatsData 
 {
-    extends Runner.java;    
+    extends Runner.java;  
+    public int avgSteps(Runner)
+    {
+    	
+    }
+    public float avgCalBurnt(Running)
+    {
+    	
+    }
+    public int avgActiveTime(Runner)
+    {
+    	
+    }
+    public float avgDistance(Running)
+    {
+    	
+    }
+    public float avgSpeed(Running)
+    {
+    	
+    }
+    public float avgHeartRate(Running)
+    {
+    	
+    }
 }
+
