906eeb9f5de3ff5f574d35dd110020a7103f7766
ljs421 <ljs421@blaster.cs.mun.ca>
2018-11-13 15:14:58 -0330

    Import Data Class

---
+++ ImportData.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import 
+
+public class ImportData extends Runner {
+	private double time = 0;
+	private double distance = 0;
+	private double altitude = 0;
+	
+	public ImportData(String file) {
+		try {
+			Scanner scanner = new Scanner(new File(file));
+	        while(scanner.hasNext()){
+	        	String str = scanner.nextLine();
+	        	String[] tokens = str.split(",");
+	        	this.time = this.time + Double.parseDouble(tokens[0]);
+	        	this.distance = this.distance + Double.parseDouble(tokens[1]);
+	        	this.altitude = Double.parseDouble(tokens[2]);
+	        }
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			scanner.close();
+			super.Steps = super.Steps + this.
+		}
+		
+        
+		
+	}
+}
