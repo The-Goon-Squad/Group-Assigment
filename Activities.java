7848a58eec5bfbccb631da8d4ec41d0585dbbc9d
tahsinsakib <43591232+tahsinsakib@users.noreply.github.com>
2018-11-14 17:17:19 -0330

    Add files via upload

---
+++ Activities.java
@@ -0,0 +1,19 @@
+public interface Activities {
+	
+	public void getCaloriesBurnt();
+	
+	public void getHeartRate();
+	
+	public void getSpeed();
+	
+	public void getPace();
+	
+	public void getDehydration();
+	
+	public void getDistance();
+	
+	public void getSteps();
+	
+	public void getActiveTime();
+	
+}
