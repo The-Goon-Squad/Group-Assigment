925be86d26ac7433eaf911d1c0a9e58237b0049c
Conrad John brown <43575435+Cjeb24@users.noreply.github.com>
2018-11-20 10:44:40 -0330

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -1,74 +1,75 @@
 
 
-//All unneeded methods and variables are commented out
-public class Running {
-	//The variables are defaulted to zero if no data was collected during the run
-	//private float CalBurn = 0;
-	//private float HeartRate = 0;
-	//private float Speed = 0;
-	//private boolean Dehydration = false;
-	private double Distance = 0;
-	//private int Steps = 0;
-	private double time = 0;
-	private double altitude = 0;
-	 // this method is used as the main setter of the class it will set all the stats of the run
-	 public void Run(double D, double T,double Alt){
-		// this.CalBurn = CB;
-		// this.HeartRate = HR;
-		// this.Speed = S;
-		 //this.Dehydration = DH;
-		 this.Distance = D;
-		// this.Steps = step;
-		 this.time = T;
-		 this.altitude = Alt;
-		    
+
+
+public class Profile {
+	private String name;
+	private String gender;
+	private String dob;
+ 	private float height;
+	private float weight;
+	private ImportData data = new ImportData("src/Tracker/file.txt");
+	
+	   
+
+    public Profile(String name, String gender, String dob, float height, float weight) {
+		this.name = name;
+		this.dob = dob;
+		this.gender = gender;
+		this.height = height;
+		this.weight = weight;
+		
 	}
-	 public void setAltitude(double a) {
-		 this.altitude = a;
-	 }
-	 /*public void setCalburn(float CB) {
-		 this.CalBurn = CB;
-	 }
-	 public void setHeartRate(float HR) {
-		 this.HeartRate = HR;
-	 }
-	 public void setSpeed(float S) {
-		 this.Speed = S;
-	 }
-	 public void setDehydration(Boolean DH) {
-		 this.Dehydration = DH;
-	 }*/
-	 public void setDistance(double d) {
-		 this.Distance = d;
-	 }
-	 /*public void setSteps(int step) {
-		 this.Steps = step;
-	 }*/
-	 public void setTime(double d) {
-		 this.time = d;
-	 }
-	 /*public float getCalBurn() {
-		 return CalBurn;
-	 }
-	 public float getHeartRate() {
-		 return HeartRate;
-	 }
-	 public float getSpeed() {
-		 return Speed;
-	 }*/
-	 public double getAltitude() {
-		 return altitude;
-	 }
-	 /*public boolean getDehydration() {
-		 return Dehydration;
-	 }*/
-	 public double getDistance() {
-		 return Distance;
-	 }
-	 /*public int getSteps() {
-		 return Steps;
-	 }*/
-	 public double getTime() {
-		 return time;
-	 }
+    
+    public void importData() {
+    	data.importData();
+    }
+    public double getDistance() {
+    	return data.getDistance();
+    }
+    public double getAltitude() {
+    	return data.getAltitude();
+    }
+    public double getTime() {
+    	return data.getTime();
+    }
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDOB(String dob) {
+		this.dob = dob;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+	
+	public void setweight(float weight) {
+		this.weight = weight;
+	}
+	
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public String getDOB(){
+		return this.dob;
+	}
+
+	public float getHeight(){
+		return this.height;
+	}
+	
+	public String getGender() {
+		return this.gender;
+	}
+
+	public float getWeight(){
+		return this.weight;
+	}
 }
