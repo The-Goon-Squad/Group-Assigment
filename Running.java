d05009928b8beca955e4b13b34df8297d3a656a4
Conrad John brown <43575435+Cjeb24@users.noreply.github.com>
2018-11-20 10:45:51 -0330

    Update Running.java

--- Running.java
+++ Running.java
@@ -1,31 +1,32 @@
 
 
+//All unneeded methods and variables are commented out
 public class Running {
 	//The variables are defaulted to zero if no data was collected during the run
-	private float CalBurn = 0;
-	private float HeartRate = 0;
-	private float Speed = 0;
-	private boolean Dehydration = false;
+	//private float CalBurn = 0;
+	//private float HeartRate = 0;
+	//private float Speed = 0;
+	//private boolean Dehydration = false;
 	private double Distance = 0;
-	private int Steps = 0;
+	//private int Steps = 0;
 	private double time = 0;
 	private double altitude = 0;
 	 // this method is used as the main setter of the class it will set all the stats of the run
-	 public void Run(float CB, float HR,float S,boolean DH,
-					float D, int step, int T){
-		 this.CalBurn = CB;
-		 this.HeartRate = HR;
-		 this.Speed = S;
-		 this.Dehydration = DH;
+	 public void Run(double D, double T,double Alt){
+		// this.CalBurn = CB;
+		// this.HeartRate = HR;
+		// this.Speed = S;
+		 //this.Dehydration = DH;
 		 this.Distance = D;
-		 this.Steps = step;
+		// this.Steps = step;
 		 this.time = T;
+		 this.altitude = Alt;
 		    
 	}
 	 public void setAltitude(double a) {
 		 this.altitude = a;
 	 }
-	 public void setCalburn(float CB) {
+	 /*public void setCalburn(float CB) {
 		 this.CalBurn = CB;
 	 }
 	 public void setHeartRate(float HR) {
@@ -36,17 +37,17 @@ public class Running {
 	 }
 	 public void setDehydration(Boolean DH) {
 		 this.Dehydration = DH;
-	 }
+	 }*/
 	 public void setDistance(double d) {
 		 this.Distance = d;
 	 }
-	 public void setSteps(int step) {
+	 /*public void setSteps(int step) {
 		 this.Steps = step;
-	 }
+	 }*/
 	 public void setTime(double d) {
 		 this.time = d;
 	 }
-	 public float getCalBurn() {
+	 /*public float getCalBurn() {
 		 return CalBurn;
 	 }
 	 public float getHeartRate() {
@@ -54,19 +55,19 @@ public class Running {
 	 }
 	 public float getSpeed() {
 		 return Speed;
-	 }
+	 }*/
 	 public double getAltitude() {
 		 return altitude;
 	 }
-	 public boolean getDehydration() {
+	 /*public boolean getDehydration() {
 		 return Dehydration;
-	 }
+	 }*/
 	 public double getDistance() {
 		 return Distance;
 	 }
-	 public int getSteps() {
+	 /*public int getSteps() {
 		 return Steps;
-	 }
+	 }*/
 	 public double getTime() {
 		 return time;
 	 }
