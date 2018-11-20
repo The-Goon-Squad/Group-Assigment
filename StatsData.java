214d02dc406961b7e8daa338742c536a8b596f0d
Conrad John brown <43575435+Cjeb24@users.noreply.github.com>
2018-11-20 10:47:37 -0330

    Update StatsData.java

--- StatsData.java
+++ StatsData.java
@@ -1,7 +1,11 @@
 
 
+import java.util.ArrayList;
+
 public class StatsData extends Running
 {
+	Running run;//Class variable of runs
+	ArrayList<Running> Runs = new ArrayList<Running>(); //an array list of all runs
  
     /*public int avgSteps(super.Steps)
     {
@@ -27,5 +31,14 @@ public class StatsData extends Running
     {
     	
     }*/
+	public void AddRun(double time,double distance,double altitude) {
+		// a method to add different Runs to the list
+		run.Run(distance, time, altitude);
+		Runs.add(run);
+	}
+	public Object ReturnRuns() { //Returns the list of runs
+		return Runs;
+	}
 }
+
 
