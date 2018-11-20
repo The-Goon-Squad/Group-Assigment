dfa9a1fa9f1e5e8cde1226888e535c193b5ab88f
Conrad John brown <43575435+Cjeb24@users.noreply.github.com>
2018-11-20 10:47:00 -0330

    Update ImportData.java

--- ImportData.java
+++ ImportData.java
@@ -1,14 +1,13 @@
-
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 
-public class ImportData extends Running {
+public class ImportData extends StatsData {
 	private double time = 0;
 	private double distance = 0;
 	private double altitude = 0;
+	private double Zero = 0;
 	private Scanner scanner;
 	
 	public ImportData(String file) {
@@ -16,19 +15,21 @@ public class ImportData extends Running {
 	
 	public void importData() {
 		try {
-			scanner = new Scanner(new File("C:\\Users\\Logan\\eclipse-workspace\\Activity Tracker\\src\\file.txt"));
+			scanner = new Scanner(new File("C:\\Users\\conra\\eclipse-workspace\\activity tracker\\src\\Input Format.csv"));
 	        while(scanner.hasNext()){
 	        	String data = scanner.nextLine();
 	        	String[] tokens = data.split(",");
 	        	this.time = this.time + Double.parseDouble(tokens[0]);
 	        	this.distance = this.distance + Double.parseDouble(tokens[1]);
 	        	this.altitude = Double.parseDouble(tokens[2]);
+	        	if (tokens[0].equals(Zero) ) {
+	        		super.AddRun(time, distance, altitude);
+	        		time = 0;
+	        		distance = 0;
+	        		altitude = 0;
+	        	}
 	        	
 	        }
-	       super.setDistance(super.getDistance() + this.distance);
-	       super.setTime((double) (super.getTime() + this.time));
-	       super.setAltitude((float) (super.getAltitude() + this.altitude));
-				
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 
